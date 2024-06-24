7a964a5ff071dc3afbee98e7efbfb7656e7a88db
Eloiza <eloizaams@yahoo.com.br>
2024-06-24 18:17:55 -0300

    Inserção de usuário com POST

--- src/main/java/com/eloizaams/workshopmongo/resources/UserResources.java
+++ src/main/java/com/eloizaams/workshopmongo/resources/UserResources.java
@@ -1,15 +1,18 @@
 package com.eloizaams.workshopmongo.resources;
 
+import java.net.URI;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.eloizaams.workshopmongo.domain.User;
 import com.eloizaams.workshopmongo.dto.UserDTO;
@@ -34,5 +37,15 @@ public class UserResources {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDTO){
+		User obj = service.fromDTO(objDTO);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build(); //retorna codigo 201 e com cabeçalho da localizacao do novo recurso criado
+	}
+	
+	
 
 }
