a448c7f099cc4ae3b8df918cdf5b8fe950f49c25
Eloiza <eloizaams@yahoo.com.br>
2024-06-24 16:47:57 -0300

    Entity User e REST funcionando

--- src/main/java/com/eloizaams/workshopmongo/WorkshopmongoApplication.java
+++ src/main/java/com/eloizaams/workshopmongo/WorkshopmongoApplication.java
@@ -1,4 +1,4 @@
-package com.eloizaams.workshpmongo;
+package com.eloizaams.workshopmongo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
