671e052e67305d4f200da5fd4f161e28d7248da1
Eloiza <eloizaams@yahoo.com.br>
2024-06-24 21:30:09 -0300

    Consulta simples com @Query

--- src/main/java/com/eloizaams/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/eloizaams/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.eloizaams.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.eloizaams.workshopmongo.domain.Post;
@@ -10,5 +11,9 @@ import com.eloizaams.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 	
+	@Query("{ title: { $regex: ?0, $options: 'i' } }") //?0 - primeiro parametro, "i" - insensitive case
+	List<Post> searchTitle (String text);
+
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
 }
